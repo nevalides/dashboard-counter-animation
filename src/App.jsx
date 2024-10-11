88e7bab3d0133ba91ea4f340bd58393b38654984
Mohammad Hafidh WM <hafidh.wildan@sg-dsa.com>
2024-10-11 13:59:46 +0700

    feat(index): adding logic to get today's time

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import Counter from './Counter';
 import './App.css'
 
 function App() {
+  const currentDate = new Date();
+  const formatDate = (date) => {
+    const todayDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+    const time = `${date.getHours()}:${date.getMinutes()}`
+
+    const days = ['Sunday', 'Monday', "Tuesday", 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return `${days[date.getDay()]}, ${todayDate} @${time}`
+  }
   return (
     <div className='image-background w-full h-full min-h-screen'>
-      <div className='body-counter w-full h-full bg-black bg-opacity-20 min-h-screen flex items-center justify-center'>
+      <div className='body-counter w-full h-full bg-black bg-opacity-20 min-h-screen flex flex-col items-center justify-center lg:gap-6 md:gap-4 gap-2'>
         <Counter />
+        <div className='text-white p-2 lg:text-2xl md:text-xl sm:text-lg text-base font-semibold leading-none'>
+          {formatDate(currentDate)}
+        </div>
       </div>
     </div>
   )
